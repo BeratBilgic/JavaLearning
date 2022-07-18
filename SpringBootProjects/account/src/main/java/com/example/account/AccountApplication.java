2cbbd2881ffb60b7a9db3522f7dd43db77623769
Berat Bilgic <bertbilgc@gmail.com>
2022-07-18 18:47:04 +0300

    create controller(api)

--- SpringBootProjects/account/src/main/java/com/example/account/AccountApplication.java
+++ SpringBootProjects/account/src/main/java/com/example/account/AccountApplication.java
@@ -1,13 +1,39 @@
 package com.example.account;
 
+import com.example.account.model.Customer;
+import com.example.account.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Clock;
 
 @SpringBootApplication
-public class AccountApplication {
+public class AccountApplication implements CommandLineRunner {
+
+	private final CustomerRepository customerRepository;
+
+	public AccountApplication(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
 	}
 
+	@Bean
+	public Clock clock() {
+		return Clock.systemUTC();
+	}
+
+	@Override
+	public void run(String... args)  {
+		Customer customer = customerRepository.save(new Customer("Tony", "Toy"));
+		Customer customer2 = customerRepository.save(new Customer("Alice", "Bob"));
+
+		System.out.println(customer);
+		System.out.println(customer2);
+
+	}
 }
