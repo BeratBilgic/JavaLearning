9affce055cd4f5d7791043bfa552fdbec98f109e
Berat Bilgic <bertbilgc@gmail.com>
2022-07-24 13:15:06 +0300

    react js added

--- SpringBootProjects/account/account-api/src/main/java/com/example/account/AccountApplication.java
+++ SpringBootProjects/account/account-api/src/main/java/com/example/account/AccountApplication.java
@@ -2,6 +2,10 @@ package com.example.account;
 
 import com.example.account.model.Customer;
 import com.example.account.repository.CustomerRepository;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,6 +24,17 @@ public class AccountApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${application-description}") String description,
+								 @Value("${application-version}") String version){
+		return new OpenAPI()
+				.info(new Info()
+						.title("Account API")
+						.version(version)
+						.description(description)
+						.license(new License().name("Account API Licence")));
 	}
 
 	@Bean
