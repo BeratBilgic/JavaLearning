c8e13ef44d1a550d4455bb18c020970535998884
Berat Bilgic <bertbilgc@gmail.com>
2023-10-12 21:58:08 +0300

    project structure converted to spring boot

---
+++ Effective-Java-3rd-Edition/src/main/java/com/example/effectivejava/Item07/Item07.java
@@ -0,0 +1,8 @@
+package com.example.effectivejava.Item07;
+
+public class Item07 {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Effective-Java-3rd-Edition/src/test/java/com/example/effectivejava/EffectivejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.effectivejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectivejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
