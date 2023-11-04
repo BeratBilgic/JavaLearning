e771840b05bbae3616e456f60cd69c2e548dde8f
Berat Bilgic <bertbilgc@gmail.com>
2023-11-04 20:42:39 +0300

    Item 16: In public classes,use accessor methods, not public fields

---
+++ Effective-Java-3rd-Edition/src/main/java/com/example/effectivejava/Item16/Item16.java
@@ -0,0 +1,8 @@
+package com.example.effectivejava.Item16;
+
+public class Item16 {
+    public static void main(String[] args){
+        Time time = new Time(12,45);
+        //time.hour = 13; // Cannot assign a value to final variable 'hour'
+    }
+}
